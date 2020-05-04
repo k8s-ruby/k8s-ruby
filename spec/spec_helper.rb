1df563559dcc5e4b5a380e10678ccff27377144c
Petrus Repo <petrus.repo@iki.fi>
2020-05-04 21:28:25 +0300

    Run only focused tests with fdescribe, fcontext and fit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,9 @@ RSpec.configure do |config|
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
+  # Run only specific tests with fdescribe, fcontext and fit
+  config.filter_run_when_matching :focus
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
