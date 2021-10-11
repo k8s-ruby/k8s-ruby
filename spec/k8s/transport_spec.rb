9c4f8639da5d8f22c4cb03e31756b47c5c4a14fd
Wai Xiong Jiang <wai.jiang@naimuri.com>
2021-10-11 09:05:04 +0100

    Added comment

--- spec/k8s/transport_spec.rb
+++ spec/k8s/transport_spec.rb
@@ -219,6 +219,7 @@ RSpec.describe K8s::Transport do
               auth_provider: {
                 name: 'fake',
                 config: {
+                  # If I take away these two lines, will this test fail? is the id-token dependant on it?
                   # 'cmd-path' => 'cat',
                   # 'cmd-args' => "#{fixture_path}/config/kubeconfig_auth_provider_data.json",
                   'id-token' => '{.credential.access_token}'
